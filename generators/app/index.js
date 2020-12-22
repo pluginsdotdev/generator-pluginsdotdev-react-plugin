0b4914439c571945dc9928b700e3510fe4911be2
Adam Berger <adam@ratchetdesigns.com>
2020-12-22 11:47:50 -0500

    refactor: lint

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,50 +1,49 @@
-'use strict';
-const nfs = require('fs');
-const path = require('path');
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const nfs = require("fs");
+const path = require("path");
+const Generator = require("yeoman-generator");
+const yosay = require("yosay");
 
 const neededData = [
   {
-    type: 'input',
-    name: 'name',
-    message: 'Plugin name'
+    type: "input",
+    name: "name",
+    message: "Plugin name"
   },
   {
-    type: 'input',
-    name: 'description',
-    message: 'Plugin description'
+    type: "input",
+    name: "description",
+    message: "Plugin description"
   },
   {
-    type: 'input',
-    name: 'hostName',
-    message: 'Host name'
+    type: "input",
+    name: "hostName",
+    message: "Host name"
   },
   {
-    type: 'input',
-    name: 'pluginPoint',
-    message: 'Plugin point'
+    type: "input",
+    name: "pluginPoint",
+    message: "Plugin point"
   },
   {
-    type: 'input',
-    name: 'gitUrl',
-    message: 'Git url'
+    type: "input",
+    name: "gitUrl",
+    message: "Git url"
   },
   {
-    type: 'input',
-    name: 'authorEmail',
-    message: 'Author email'
+    type: "input",
+    name: "authorEmail",
+    message: "Author email"
   },
   {
-    type: 'input',
-    name: 'hostPluginPointPropsFile',
-    message: 'Typescript file exporting PluginProps type'
+    type: "input",
+    name: "hostPluginPointPropsFile",
+    message: "Typescript file exporting PluginProps type"
   },
   {
-    type: 'input',
-    name: 'hostFixturesDir',
-    message: 'Directory containing a file for each host fixture'
+    type: "input",
+    name: "hostFixturesDir",
+    message: "Directory containing a file for each host fixture"
   }
 ];
 
@@ -52,25 +51,22 @@ module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
 
-    neededData.forEach(({type, name, message}) => {
-      this.option(
-        name,
-        {
-          desc: message,
-          type: String
-        }
-      );
+    neededData.forEach(({ name, message }) => {
+      this.option(name, {
+        desc: message,
+        type: String
+      });
     });
   }
 
   prompting() {
-    this.log(
-      yosay(`Your plugin awaits!`)
-    );
+    this.log(yosay(`Your plugin awaits!`));
 
     const prompts = neededData
-      .filter(({name}) => !this.options[name])
-      .map(prompt => Object.assign({}, prompt, { message: prompt.message + ':' }));
+      .filter(({ name }) => !this.options[name])
+      .map(prompt =>
+        Object.assign({}, prompt, { message: prompt.message + ":" })
+      );
 
     return this.prompt(prompts).then(props => {
       this.props = Object.assign({}, this.options, props);
@@ -78,84 +74,85 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    const hostPluginPointPropsFile = path.resolve(this.props.hostPluginPointPropsFile);
+    const hostPluginPointPropsFile = path.resolve(
+      this.props.hostPluginPointPropsFile
+    );
     const hostFixturesDir = path.resolve(this.props.hostFixturesDir);
-    const hostFixturesDirGlob = path.join(hostFixturesDir, '*');
+    const hostFixturesDirGlob = path.join(hostFixturesDir, "*");
 
     nfs.mkdirSync(this.props.name);
     this.destinationRoot(this.destinationPath(this.props.name));
 
-    // do this first, we want to overwrite if there's anything to overwrite
-    this.fs.copy(
-      hostFixturesDirGlob,
-      this.destinationPath('fixtures')
-    );
+    // Do this first, we want to overwrite if there's anything to overwrite
+    this.fs.copy(hostFixturesDirGlob, this.destinationPath("fixtures"));
 
     const fixtures = nfs.readdirSync(hostFixturesDir);
     const fixtureData = fixtures.map(f => {
-      const fileWithoutExt = f.replace(/\.[^.]+$/, '');
-      const exportName = fileWithoutExt.replace(/[^A-Za-z0-9_]+/g, '_');
+      const fileWithoutExt = f.replace(/\.[^.]+$/, "");
+      const exportName = fileWithoutExt.replace(/[^A-Za-z0-9_]+/g, "_");
       return {
         exportName,
         fileWithoutExt,
-        storyName: exportName + 'Story'
-      }
+        storyName: exportName + "Story"
+      };
     });
 
     const urlSafeName = x => x; // TODO
-    const ctx = Object.assign(
-      {},
-      this.props,
-      {
-        fixtures: fixtureData,
-        pluginUrl: `https://plugins.dev/h/${urlSafeName(this.props.hostName)}/${urlSafeName(this.props.pluginPoint)}/${urlSafeName(this.props.name)}`,
-        pluginPointUrl: `https://plugins.dev/h/${urlSafeName(this.props.hostName)}/${urlSafeName(this.props.pluginPoint)}`,
-        hostUrl: `https://plugins.dev/h/${urlSafeName(this.props.hostName)}`
-      }
-    );
+    const ctx = Object.assign({}, this.props, {
+      fixtures: fixtureData,
+      pluginUrl: `https://plugins.dev/h/${urlSafeName(
+        this.props.hostName
+      )}/${urlSafeName(this.props.pluginPoint)}/${urlSafeName(
+        this.props.name
+      )}`,
+      pluginPointUrl: `https://plugins.dev/h/${urlSafeName(
+        this.props.hostName
+      )}/${urlSafeName(this.props.pluginPoint)}`,
+      hostUrl: `https://plugins.dev/h/${urlSafeName(this.props.hostName)}`
+    });
 
     const staticFiles = [
-      '_gitignore',
-      'docker-compose.yml',
-      'jest.config.js',
-      'tsconfig.json',
-      '.storybook'
+      "_gitignore",
+      "docker-compose.yml",
+      "jest.config.js",
+      "tsconfig.json",
+      ".storybook"
     ];
     const tplFiles = [
-      'README.md',
-      'plugin.yml',
-      'package.json',
-      'src/index.ts',
-      'src/plugin.tsx',
-      'fixtures/index.ts',
-      'stories/plugin.stories.tsx',
-      'stories/readme.stories.mdx',
-      'test/fixtures.test.tsx'
+      "README.md",
+      "plugin.yml",
+      "package.json",
+      "src/index.ts",
+      "src/plugin.tsx",
+      "fixtures/index.ts",
+      "stories/plugin.stories.tsx",
+      "stories/readme.stories.mdx",
+      "test/fixtures.test.tsx"
     ];
 
     staticFiles.forEach(f =>
       this.fs.copy(
         this.templatePath(f),
-        this.destinationPath(f.replace(/^_/, '.'))
+        this.destinationPath(f.replace(/^_/, "."))
       )
     );
 
     tplFiles.forEach(f =>
       this.fs.copyTpl(
         this.templatePath(f),
-        this.destinationPath(f.replace(/^_/, '.')),
+        this.destinationPath(f.replace(/^_/, ".")),
         ctx
       )
     );
 
     this.fs.copy(
       hostPluginPointPropsFile,
-      this.destinationPath('src/plugin-props.ts')
+      this.destinationPath("src/plugin-props.ts")
     );
   }
 
   install() {
-    if ( !this.props.skipInstall ) {
+    if (!this.props.skipInstall) {
       this.installDependencies({ npm: true, bower: false });
     }
   }
